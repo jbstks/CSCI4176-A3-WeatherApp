fdfb905917f111dc918b35cf700d7b090e0e6756
Joanna <18332426+jbstks@users.noreply.github.com>
2018-10-25 14:18:23 -0300

    Images and layout of app

--- app/src/main/java/com/csci4176/jbstks/weatherapp/MainActivity.java
+++ app/src/main/java/com/csci4176/jbstks/weatherapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.csci4176.jbstks.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * ALL weather icons made by iconixar [https://www.flaticon.com/authors/iconixar] from www.flaticon.com.
+ * I am using them for free with attribution of the author (above) as advised in the article here
+ * [https://support.flaticon.com/hc/en-us/articles/202798341-Are-Flaticon-contents-for-free-and-where-can-I-use-them-]
+ *
+ * Joanna Bistekos (B00710704)
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
